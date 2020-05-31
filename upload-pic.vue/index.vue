76a31250dd3e21fbba99799daead234501b2904f
关于 <372301467@qq.com>
2020-05-31 15:47:08 +0800

    update

--- upload-pic.vue/index.vue
+++ upload-pic.vue/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="upload-pic-container">
     <input type="file" ref="input" @change="upload" />
-    <img class="add-icon" src="https://gitee.com/dggy/picpic/raw/master/icon/add.png" @click="$refs.input.click()" />
+    <div class="add-icon-wrapper" @click="$refs.input.click()">
+      <img src="https://gitee.com/dggy/picpic/raw/master/icon/add.png" />
+    </div>
     <div class="target" v-if="value">
-      <img :src="imgBaseUrl + value" />
-      <img class="refresh-icon" src="https://gitee.com/dggy/picpic/raw/master/icon/refresh.png" @click="$refs.input.click()" />
+      <img class="target-img" :src="imgBaseUrl + value" />
+      <div class="refresh-icon-wrapper" @click="$refs.input.click()">
+        <img src="https://gitee.com/dggy/picpic/raw/master/icon/refresh.png" />
+      </div>
     </div>
   </div>
 </template>
